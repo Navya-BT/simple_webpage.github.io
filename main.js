84dd47bf6c2612bb8be9753dbeaf36821ba2e131
Navya-BT <75115350+Navya-BT@users.noreply.github.com>
2021-09-15 08:53:11 +0530

    main.js

--- main.js
+++ main.js
@@ -14,3 +14,22 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
    
+loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        // Perform your AJAX/Fetch login
+
+        setFormMessage(loginForm, "error", "Invalid username/password combination");
+    });
+    document.querySelectorAll(".form__input").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+                setInputError(inputElement, "Username must be at least 10 characters in length");
+            }
+        });
+
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
+    });
+});
